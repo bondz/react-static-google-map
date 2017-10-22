b17559c550c91164dceef4dc415c03ee19e4b6ff
Bond <lovedammy77@yahoo.com>
2017-10-22 12:40:04 +0100

    Add component tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -1,1 +1,106 @@
-// Add tests, don't be lazy.
+import React from 'react';
+import Enzyme, { shallow, mount, render } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { StaticGoogleMap, Marker, Path, Direction } from '../src/index';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('Components', () => {
+  test('It exports Components', () => {
+    expect(StaticGoogleMap).toBeDefined();
+    expect(Marker).toBeDefined();
+    expect(Path).toBeDefined();
+    expect(Direction).toBeDefined();
+  });
+
+  test('Marker and Path component have Group property', () => {
+    expect(Marker.Group).toBeDefined();
+    expect(Path.Group).toBeDefined();
+  });
+
+  test('It throws if size property is not set', () => {
+    console.error = jest.fn();
+    expect(() => mount(<StaticGoogleMap />)).toThrowError();
+    expect(console.error).toHaveBeenCalled();
+  });
+
+  test('It throws if no child is present', () => {
+    console.error = jest.fn();
+    expect(() => mount(<StaticGoogleMap size="200x200" />)).toThrow();
+    expect(console.error).toHaveBeenCalled();
+  });
+
+  test('It renders an image with default props', done => {
+    const wrapper = mount(
+      <StaticGoogleMap size="test">
+        <Marker location="location" />
+      </StaticGoogleMap>
+    );
+
+    setTimeout(() => {
+      wrapper.update();
+      const node = wrapper.getDOMNode();
+      expect(node.localName).toBe('img');
+      expect(node).toMatchSnapshot();
+      done();
+    });
+  });
+
+  test.skip('It renders composite child elements', done => {
+    const RedMarker = ({ color, ...props }) => (
+      <Marker color="red" {...props} />
+    );
+
+    const wrapper = mount(
+      <StaticGoogleMap size="test">
+        <RedMarker location="test" />
+      </StaticGoogleMap>
+    );
+
+    setTimeout(() => {
+      wrapper.update();
+      const node = wrapper.getDOMNode();
+      expect(node.getAttribute('src').includes('marker')).toBe(true);
+      done();
+    }, 15);
+  });
+
+  test('It calls onGenerate method when a URL is successfully generated', done => {
+    const onGenerate = jest.fn();
+    const wrapper = mount(
+      <StaticGoogleMap size="test" onGenerate={onGenerate}>
+        <Marker location="test" />
+        <Path points="test" />
+      </StaticGoogleMap>
+    );
+
+    setTimeout(() => {
+      wrapper.update();
+      const node = wrapper.getDOMNode();
+      const url = node.getAttribute('src');
+      expect(url).toBeTruthy();
+      expect(onGenerate).toHaveBeenCalledTimes(1);
+      expect(onGenerate).toHaveBeenCalledWith(url);
+      done();
+    });
+  });
+
+  test('It passes unknown props to the rendered component', done => {
+    const title = 'A test title';
+    const alt = 'An alternate property';
+    const wrapper = mount(
+      <StaticGoogleMap size="test" title={title} alt={alt}>
+        <Marker location="test" />
+      </StaticGoogleMap>
+    );
+
+    setTimeout(() => {
+      wrapper.update();
+      const node = wrapper.getDOMNode();
+      expect(node.localName).toBe('img');
+      expect(node.getAttribute('title')).toBe(title);
+      expect(node.getAttribute('alt')).toBe(alt);
+      done();
+    });
+  });
+});
